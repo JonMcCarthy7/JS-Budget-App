21ebfd72a031787abcfef05ff38698f0c3cfed05
Jon McCarthy <jtmcc93@gmail.com>
2018-05-17 11:16:13 -0700

    return getDOMstrings for use in the Global App Controller, add inputBtn to DOMstrings object

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var UIController = (function() {
   var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
-    inputValue: ".add__value"
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
   };
   return {
     getInput: function() {
@@ -15,12 +16,16 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UICtrl.getInput();
@@ -28,9 +33,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
+    console.log(input);
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(evenet) {
     if (event.keyCode === 13 || event.which === 13) {
