cb988a1ddc1c6711aebefb165aac18e8ae7a0837
Jon McCarthy <jtmcc93@gmail.com>
2018-05-24 16:00:42 -0700

    add public method deleteItem in budgetCtrl

--- app.js
+++ app.js
@@ -55,6 +55,20 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type.splice(index, 1)];
+      }
+    },
+
     getBudget: function() {
       return {
         budget: data.budget,
@@ -79,6 +93,7 @@ var budgetController = (function() {
         data.percentage = -1;
       }
     },
+
     testing: function() {
       console.log(data);
     }
@@ -218,9 +233,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     if (itemID) {
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
-
+      ID = parseInt(splitID[1]);
+      parseInt;
       // 1. Delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
 
       // 2. Delete the item from the UI
 
